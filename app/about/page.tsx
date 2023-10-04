9bd1a4eed34ae4b686cb2191a5d88860b577bad8
Isaac D'Césares <isaac.dcesares@gmail.com>
2023-10-03 22:38:30 -0300

    Load md

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import myMarkdownFile from './data.md';
 
+import Hero from '@/components/hero'
+
 const MyComponent = () => {
   return (
-    <div>
+    <div className='max-w-6xl mx-auto px-4 sm:px-6 relative'>
       <ReactMarkdown>{myMarkdownFile}</ReactMarkdown>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: ['raw-loader', 'markdown-loader']
+
+      });
+      return config;
+    }
+  }
